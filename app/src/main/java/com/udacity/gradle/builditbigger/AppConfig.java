05756793332a8ad6d751b5eca0c61d3473ce2247
harshadpune <harshadpune@gmail.com>
2018-07-23 00:58:52 +0530

    Build It Bigger : Interstitial Ad included

--- app/src/main/java/com/udacity/gradle/builditbigger/AppConfig.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/AppConfig.java
@@ -4,4 +4,5 @@ public class AppConfig {
         public static final String FLAVOR = "free";
         public static final String FREE = "free";
         public static final String PAID = "paid";
+        public static final String AD_TEST_KEY = "ca-app-pub-3940256099942544/1033173712";
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,14 +4,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar pbLoading;
+    private InterstitialAd interstitialAd;
 
 
     @Override
@@ -23,32 +25,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void initComponents() {
         pbLoading = (ProgressBar) findViewById(R.id.pbLoading);
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(AppConfig.AD_TEST_KEY);       //test key for Ad
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+        interstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                getJokeFromServer();
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     public void tellJoke(View view) {
+        if(getResources().getString(R.string.flavor).equalsIgnoreCase(AppConfig.FREE)) {
+            interstitialAd.show();
+        }else if(getResources().getString(R.string.flavor).equalsIgnoreCase(AppConfig.PAID)){
+            getJokeFromServer();
+        }
+    }
+
+    public void getJokeFromServer(){
         EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
         pbLoading.setVisibility(View.VISIBLE);
         endpointsAsyncTask.setListener(new EndpointsAsyncTask.EndpointsAsyncTaskListener() {
@@ -57,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 pbLoading.setVisibility(View.GONE);
             }
         });
-        endpointsAsyncTask.execute(new Pair<Context, String>(this, ""));
+        endpointsAsyncTask.execute(new Pair<Context, String>(MainActivity.this, ""));
     }
 
 }
