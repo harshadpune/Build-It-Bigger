e3d1047660e43b02847524d5fd7d66bec206b932
harshadpune <harshadpune@gmail.com>
2018-07-23 00:08:15 +0530

    Build It Bigger : Free and Paid apps flavors created.

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/AppConfig.java
@@ -0,0 +1,7 @@
+package com.udacity.gradle.builditbigger;
+
+public class AppConfig {
+        public static final String FLAVOR = "free";
+        public static final String FREE = "free";
+        public static final String PAID = "paid";
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ProgressBar pbLoading;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
